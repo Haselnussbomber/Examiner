949225e913bf72d2c1b4b20b5f3546470d38b228
Haselnussbomber <mail@haselnussbomber.de>
2019-05-21 15:19:30 +0200

    remove tab tooltips

--- ExaminerTabMixin.lua
+++ ExaminerTabMixin.lua
@@ -4,7 +4,3 @@ function ExaminerTabMixin:OnClick()
 	PlaySound(SOUNDKIT.IG_CHARACTER_INFO_TAB);
 	Examiner:SwitchTabs(self:GetID());
 end
-
-function ExaminerTabMixin:OnLeave()
-	GameTooltip_Hide(self);
-end
